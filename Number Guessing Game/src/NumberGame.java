f3c66998d8cffd03aaf4ea09fb0608f993dd5cc2
smgzach <zm989392@gmail.com>
2025-05-19 12:06:58 -0400

    mostly finished logic for easy difficulty

--- Number Guessing Game/src/NumberGame.java
+++ Number Guessing Game/src/NumberGame.java
@@ -1,4 +1,5 @@
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGame {
@@ -7,6 +8,7 @@ public class NumberGame {
 	{
 		int gameMode = 0;
 		int chances = 0;
+		Random rnum = new Random();
 		Scanner in = new Scanner(System.in);
 		
 		displayWelcome();
@@ -25,8 +27,53 @@ public class NumberGame {
 				switch(gameMode)
 				{
 				case(1):
-					System.out.println("Easy");
+					System.out.println("You have selected Easy difficulty! Good luck!");
+					Thread.sleep(1000);
+					chances = 8;
+					int randomNum = rnum.nextInt(100);
+					int userNum = -1;
+					while(chances > 0)
+					{
+						System.out.println("Enter your guess: ");
+						Thread.sleep(1000);
+						try
+						{
+							String input = in.nextLine();
+							userNum = Integer.parseInt(input);
+							chances--;
+							if(userNum == randomNum)
+							{
+								System.out.println("Congrats! You guessed correctly.");
+								break;
+							}
+							else
+							{
+								String greaterOrLess = "";
+								int diff = userNum - randomNum;
+								if(diff < 0)
+								{
+									greaterOrLess = "greater";
+								}
+								else
+								{
+									greaterOrLess = "less";
+								}
+								System.out.println("Incorrect! The number is " + greaterOrLess + " than " + userNum + ".");
+								Thread.sleep(1000);
+							}
+						}
+						catch (NumberFormatException e)
+						{
+							System.out.println("Invalid input, please enter a valid integer number.");
+							Thread.sleep(1000);
+						}
+					}
+					if(chances <= 0 && userNum != randomNum)
+					{
+						System.out.println("You ran out of guesses!");
+					}
 					break;
+					
 				case(2):
 					System.out.println("Medium");
 					break;
@@ -46,7 +93,7 @@ public class NumberGame {
 				Thread.sleep(1000);
 			}
 		}
-
+		
 	}
 	
 	public static void displayWelcome() throws InterruptedException
